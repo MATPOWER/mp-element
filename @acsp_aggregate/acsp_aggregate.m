36d8406a0dd2fe70f086532c53e76973b6445ab4
Ray Zimmerman <rz10@cornell.edu>
2020-03-24 16:09:05 -0600

    Add acsp_aggregate methods: add_opf_node_balance_constraints(), x2x_()
    Update nodal_power_balance_hess()

--- @acsp_aggregate/acsp_aggregate.m
+++ @acsp_aggregate/acsp_aggregate.m
@@ -32,24 +32,44 @@ classdef acsp_aggregate < ac_aggregate & acsp_model
             obj.set_types.vm = 'VOLTAGE MAG VARS (vm)';
         end
 
-        function d2G = nodal_power_balance_hess(obj, xx, lam)
+        function x_ = x2x_(obj, x)
+            %% convert real to complex x
+            nx_ = size(x, 1) / 2;   %% number of state vars
+            nv_ = obj.nv / 2;       %% number of voltage vars (sysx=1)
+            nz_ = nx_ - nv_;        %% number of non-voltage state vars
+            va = x(1:nv_, :);       b = nv_;
+            vm = x(b+1:b+nv_, :);   b = b + nv_;
+            zr = x(b+1:b+nz_, :);   b = b + nz_;
+            zi = x(b+1:b+nz_, :);
+            x_ = [vm .* exp(1j*va); zr+1j*zi];
+        end
+
+        function d2G = nodal_power_balance_hess(obj, x_, lam)
             %% node incidence matrix
             Ct = obj.getC('tr');
 
             %% get port power injection hessians
-            d2G = obj.port_inj_power_hess(xx, Ct * lam);
+            d2G = obj.port_inj_power_hess(x_, Ct * lam);
         end
 
-        function d2G = opf_power_balance_hess(obj, xc, lam)
-            xx = [xc{2} .* exp(1j*xc{1}); xc{3}+1j*xc{4}];
+        function d2G = opf_power_balance_hess(obj, x, lam)
+            x_ = obj.x2x_(x);           %% convert real to complex x
             nlam = length(lam) / 2;
             lamP = lam(1:nlam);
             lamQ = lam((1:nlam)+nlam);
 
-            d2Gr = obj.nodal_power_balance_hess(xx, lamP);
-            d2Gi = obj.nodal_power_balance_hess(xx, lamQ);
+            d2Gr = obj.nodal_power_balance_hess(x_, lamP);
+            d2Gi = obj.nodal_power_balance_hess(x_, lamQ);
 
             d2G = real(d2Gr) + imag(d2Gi);
+        end
+
+        function add_opf_node_balance_constraints(obj, om)
+            %% power balance constraints
+            nn = obj.node.N;            %% number of nodes
+            fcn_mis = @(x)opf_power_balance_fcn(obj, x);
+            hess_mis = @(x, lam)opf_power_balance_hess(obj, x, lam);
+            om.add_nln_constraint({'Pmis', 'Qmis'}, [nn;nn], 1, fcn_mis, hess_mis);
         end
     end     %% methods
 end         %% classdef
