82c47b3abd3dd102540526032bb0a03cdcdfd226
Ray Zimmerman <rz10@cornell.edu>
2020-03-24 16:01:21 -0600

    Add mp_gen/add_opf_costs()

--- mp_gen.m
+++ mp_gen.m
@@ -45,5 +45,43 @@ classdef mp_gen < mp_element
             sidx = asm.state.data.ID2idx.(obj.name);    %% state indexes
             obj.D = { sparse(sidx, 1:ng, 1, nz, ng) };
         end
+
+        %%-----  OPF methods  -----
+        function add_opf_costs(obj, asm, om, mpc, mpopt)
+            %% find/prepare polynomial generator costs
+            [PW_LINEAR, POLYNOMIAL, MODEL, STARTUP, SHUTDOWN, NCOST, COST] = idx_cost;
+            ng = obj.nk;
+            cpg = [];
+            cqg = [];
+            [pcost qcost] = pqcost(mpc.gencost, ng);
+            ip0 = find(pcost(:, MODEL) == POLYNOMIAL & pcost(:, NCOST) == 1);   %% constant
+            ip1 = find(pcost(:, MODEL) == POLYNOMIAL & pcost(:, NCOST) == 2);   %% linear
+            ip2 = find(pcost(:, MODEL) == POLYNOMIAL & pcost(:, NCOST) == 3);   %% quadratic
+            ip3 = find(pcost(:, MODEL) == POLYNOMIAL & pcost(:, NCOST) > 3);    %% cubic or greater
+            if ~isempty(ip3)
+                error('mp_gen/add_opf_costs: polynomial generator costs greater than quadratic order not yet implemented');
+            end
+            if ~isempty(ip2) || ~isempty(ip1) || ~isempty(ip0)
+                kpg = zeros(ng, 1);
+                cpg = zeros(ng, 1);
+                if ~isempty(ip2)
+                    Qpg = zeros(ng, 1);
+                    Qpg(ip2) = 2 * pcost(ip2, COST) * mpc.baseMVA^2;
+                    cpg(ip2) = cpg(ip2) + pcost(ip2, COST+1) * mpc.baseMVA;
+                    kpg(ip2) = kpg(ip2) + pcost(ip2, COST+2);
+                else
+                    Qpg = [];   %% no quadratic terms
+                end
+                if ~isempty(ip1)
+                    cpg(ip1) = cpg(ip1) + pcost(ip1, COST) * mpc.baseMVA;
+                    kpg(ip1) = kpg(ip1) + pcost(ip1, COST+1);
+                end
+                if ~isempty(ip0)
+                    kpg(ip0) = kpg(ip0) + pcost(ip0, COST);
+                end
+            end
+            om.add_quad_cost('polPg', Qpg, cpg, kpg, {'Pg'});
+            
+        end
     end     %% methods
 end         %% classdef
