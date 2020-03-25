7d6c7bb4ce395fbb42f7a0df22b5beabf92b8b27
Ray Zimmerman <rz10@cornell.edu>
2020-03-25 16:51:37 -0600

    Move solve_opf() from @acsp_aggregate to @mp_aggregate

--- @mp_aggregate/solve_opf.m
+++ @mp_aggregate/solve_opf.m
@@ -29,13 +29,21 @@ end
 om = obj.setup_opf(mpc, mpopt);
 
 %% solve it
-opt = mpopt2nlpopt(mpopt, om.problem_type(), 'DEFAULT');
+if strcmp(om.problem_type(), 'NLP')
+    opt = mpopt2nlpopt(mpopt, om.problem_type(), 'DEFAULT');
+else
+    opt = mpopt2qpopt(mpopt, om.problem_type(), 'DEFAULT');
+end
 [x, f, eflag, output, lambda] = om.solve(opt);
 success = (eflag > 0);
 
 % om
 % vv = om.get_idx('var');
-% x(vv.i1.Pg:vv.iN.Pg)
+% x(vv.i1.Pg:vv.iN.Pg) * mpc.baseMVA
 % keyboard
 
-i = output.iterations;
+if isfield(output, 'iterations')
+    i = output.iterations;
+else
+    i = -1;
+end
