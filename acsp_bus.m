32cedbcb8606fa2e7219bcbbe6dbafc722d38ea0
Ray Zimmerman <rz10@cornell.edu>
2020-03-30 16:20:23 -0600

    Fix bug in OPF constraint for non-zero reference angles.

--- acsp_bus.m
+++ acsp_bus.m
@@ -27,11 +27,13 @@ classdef acsp_bus < mp_bus & acsp_model
             Va0   = mpc.bus(:, VA) * pi/180;
             Vm0   = mpc.bus(:, VM);
             Vamax = Inf(nb, 1);
+            Vamin = -Inf(nb, 1);
             k = find(mpc.bus(:, BUS_TYPE) == REF);
             Vamax(k) = Va0(k);
+            Vamin(k) = Va0(k);
             Vmin = mpc.bus(:, VMIN);
             Vmax = mpc.bus(:, VMAX);
-            asm.add_var('va', 'Va', nb, Va0, -Vamax, Vamax);
+            asm.add_var('va', 'Va', nb, Va0, Vamin, Vamax);
             asm.add_var('vm', 'Vm', nb, Vm0, Vmin, Vmax);
         end
     end     %% methods
