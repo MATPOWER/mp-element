8ee89d78040f45d682bc1e5f2533fdcbfab912c4
Ray Zimmerman <rz10@cornell.edu>
2020-04-08 16:51:27 -0600

    Add voltage related nonlinear OPF constraints for cartesian voltages.
    - voltage angle reference equality
    - voltage magnitude equalities
    - voltage magnitude bounds
    - branch angle difference limits

--- acc_branch.m
+++ acc_branch.m
@@ -1,8 +1,9 @@
 classdef acc_branch < ac_branch & acc_model
 
 %   MATPOWER
-%   Copyright (c) 2019-2020, Power Systems Engineering Research Center (PSERC)
+%   Copyright (c) 2018-2020, Power Systems Engineering Research Center (PSERC)
 %   by Ray Zimmerman, PSERC Cornell
+%   and Baljinnyam Sereeter, Delft University of Technology
 %
 %   This file is part of MATPOWER.
 %   Covered by the 3-clause BSD License (see LICENSE file for details).
@@ -10,21 +11,67 @@ classdef acc_branch < ac_branch & acc_model
 
 
     methods
+        function [h, dh] = ang_diff_fcn(obj, xx, Aang, lang, uang)
+            %% unpack data
+            [vr, vi] = deal(xx{:});
+
+            %% compute branch angle difference
+            va = angle(vr + 1j* vi);
+            Ax = Aang * va;
+            h = [ lang - Ax;
+                  Ax - uang ];
+
+            if nargout > 1
+                %% compute partials of branch angle difference w.r.t vr and vi
+                nn = length(vr);
+                vm2 = vr.^2 + vi.^2;
+                Aangdva_dvr = Aang * sparse(1:nn, 1:nn, -vi./vm2, nn, nn);
+                Aangdva_dvi = Aang * sparse(1:nn, 1:nn,  vr./vm2, nn, nn);
+                dh = [ -Aangdva_dvr -Aangdva_dvi;   %% h w.r.t vr, vi
+                        Aangdva_dvr  Aangdva_dvi ];
+            end
+        end
+
+        function d2H = ang_diff_hess(obj, xx, lambda, Aang)
+            %% unpack data
+            [vr, vi] = deal(xx{:});
+            nn = length(vr);
+            
+            %% evaluate Hessian of voltage magnitude^2 function
+            n = length(lambda) / 2;
+            if n
+                lam = lambda(n+1:2*n) - lambda(1:n);    %% lam_ub - lam_lb
+            else
+                lam = zeros(0,1);
+            end
+
+            vr2 = vr.^2;
+            vi2 = vi.^2;
+
+            lam_vm4 = (Aang' * lam) ./ (vr2 + vi2).^2;
+
+            h_rr = sparse(1:nn, 1:nn, 2 * lam_vm4 .*  vr .* vi,   nn, nn);
+            h_ri = sparse(1:nn, 1:nn,     lam_vm4 .* (vi2 - vr2), nn, nn);
+
+            %% construct Hessian
+            d2H = [ h_rr  h_ri;
+                    h_ri -h_rr ];
+        end
+
         function add_opf_constraints(obj, asm, om, mpc, mpopt)
             %% call parent
             add_opf_constraints@ac_branch(obj, asm, om, mpc, mpopt);
 
-%             %% define named indices into data matrices
-%             [F_BUS, T_BUS, BR_R, BR_X, BR_B, RATE_A, RATE_B, RATE_C, ...
-%                 TAP, SHIFT, BR_STATUS, PF, QF, PT, QT, MU_SF, MU_ST, ...
-%                 ANGMIN, ANGMAX, MU_ANGMIN, MU_ANGMAX] = idx_brch;
-% 
-%             %% branch voltage angle difference limits
-%             nb = size(mpc.bus, 1);      %% number of buses
-%             [Aang, lang, uang, iang] = makeAang(mpc.baseMVA, mpc.branch, nb, mpopt);
-% %             nang = length(iang);
-%             om.add_lin_constraint('ang', Aang, lang, uang, {'Va'});
-%             om.userdata.iang = iang;
+            %% branch angle difference limits
+            nb = size(mpc.bus, 1);
+            [Aang, lang, uang, iang]  = makeAang(mpc.baseMVA, mpc.branch, nb, mpopt);
+            nang = length(iang);
+            if nang
+                fcn_ang = @(xx)ang_diff_fcn(obj, xx, Aang, lang, uang);
+                hess_ang = @(xx, lam)ang_diff_hess(obj, xx, lam, Aang);
+                om.add_nln_constraint({'angL', 'angU'}, [nang;nang], 0, fcn_ang, hess_ang, {'Vr', 'Vi'});
+            end
+            om.userdata.iang = iang;
         end
     end     %% methods
 end         %% classdef
--- acc_bus.m
+++ acc_bus.m
@@ -1,8 +1,9 @@
 classdef acc_bus < mp_bus & acc_model
 
 %   MATPOWER
-%   Copyright (c) 2019-2020, Power Systems Engineering Research Center (PSERC)
+%   Copyright (c) 2018-2020, Power Systems Engineering Research Center (PSERC)
 %   by Ray Zimmerman, PSERC Cornell
+%   and Baljinnyam Sereeter, Delft University of Technology
 %
 %   This file is part of MATPOWER.
 %   Covered by the 3-clause BSD License (see LICENSE file for details).
@@ -19,10 +20,152 @@ classdef acc_bus < mp_bus & acc_model
                 VA, BASE_KV, ZONE, VMAX, VMIN, LAM_P, LAM_Q, MU_VMAX, MU_VMIN] = idx_bus;
 
             nb = obj.nk;
-            V0 = mpc.bus(:, VM) .* exp(1j * mpc.bus(:, VA) * pi/180);
-            Vclim = 1.1 * mpc.bus(:, VMAX);
-            asm.add_var('vr', 'Vr', nb, real(V0), -Vclim, Vclim);
-            asm.add_var('vi', 'Vi', nb, imag(V0), -Vclim, Vclim);
+            v0 = mpc.bus(:, VM) .* exp(1j * mpc.bus(:, VA) * pi/180);
+            vclim = 1.1 * mpc.bus(:, VMAX);
+            asm.add_var('vr', 'Vr', nb, real(v0), -vclim, vclim);
+            asm.add_var('vi', 'Vi', nb, imag(v0), -vclim, vclim);
+        end
+
+        function [g, dg] = va_fcn(obj, xx, idx, lim)
+            %% lim can be a scalar value for equality constraint or
+            %% upper bound on va, or a cell array with {vamin, vamax}
+            %% for double bounds
+
+            %% unpack data
+            [vr, vi] = deal(xx{:});
+
+            %% compute voltage angle mismatch
+            va = angle(vr(idx) + 1j* vi(idx));
+            if iscell(lim)
+                g = [ lim{1} - va;
+                      va - lim{2}   ];
+            else
+                g = va - lim;
+            end
+
+            if nargout > 1
+                %% compute partials of voltage angle w.r.t vr and vi
+                n = length(idx);
+                nn = length(vr);
+                vm2 = vr(idx).^2 + vi(idx).^2;
+                dva_dvr = sparse(1:n, idx, -vi(idx) ./ vm2, n, nn);
+                dva_dvi = sparse(1:n, idx,  vr(idx) ./ vm2, n, nn);
+                if iscell(lim)
+                    dg = [ -dva_dvr -dva_dvi;       %% va w.r.t vr, vi
+                            dva_dvr  dva_dvi  ];    %% va w.r.t vr, vi
+                else
+                    dg = [dva_dvr dva_dvi];         %% va w.r.t vr, vi
+                end
+            end
+        end
+
+        function d2G = va_hess(obj, xx, lam, idx)
+            %% unpack data
+            [vr, vi] = deal(xx{:});
+            n = length(idx);
+            nn = length(vr);
+
+            %% evaluate Hessian of voltage angle function
+            vvr = vr(idx);
+            vvi = vi(idx);
+            vvr2 = vvr.^2;
+            vvi2 = vvi.^2;
+            vvm4 = (vvr2 + vvi2).^2;
+            if length(lam) == n     %% upper bound or equality
+                lamvm4 = lam ./ vvm4;
+            else                    %% doubly bounded (use lam_ub-lam_lb)
+                lamvm4 = (lam(n+1:2*n) - lam(1:n)) ./ vvm4;
+            end
+            d2vref_rr = sparse(idx, idx, 2 * lamvm4 .*  vvr .* vvi,   nn, nn);
+            d2vref_ri = sparse(idx, idx,     lamvm4 .* (vvi2 - vvr2), nn, nn);
+
+            %% construct Hessian
+            d2G = [ d2vref_rr  d2vref_ri;
+                    d2vref_ri -d2vref_rr ];
+        end
+
+        function [g, dg] = vm2_fcn(obj, xx, idx, lim)
+            %% lim can be a scalar value for equality constraint or
+            %% upper bound on vm^2, or a cell array with {vm2min, vm2max}
+            %% for double bounds
+
+            %% unpack data
+            [vr, vi] = deal(xx{:});
+
+            %% compute voltage magnitude^2 mismatch
+            vm2 = vr(idx).^2 + vi(idx).^2;
+            if iscell(lim)
+                g = [ lim{1} - vm2;
+                      vm2 - lim{2}   ];
+            else
+                g = vm2 - lim;
+            end
+
+            if nargout > 1
+                %% compute partials of voltage magnitude^2 w.r.t vr and vi
+                n = length(idx);
+                nn = length(vr);
+                dvm_dvr = sparse(1:n, idx, 2 * vr(idx), n, nn);
+                dvm_dvi = sparse(1:n, idx, 2 * vi(idx), n, nn);
+                if iscell(lim)
+                    dg = [ -dvm_dvr -dvm_dvi;
+                            dvm_dvr  dvm_dvi  ];    %% vm2 w.r.t vr, vi
+                else
+                    dg = [ dvm_dvr dvm_dvi ];       %% vm2 w.r.t vr, vi
+                end
+            end
+        end
+
+        function d2G = vm2_hess(obj, xx, lam, idx)
+            %% unpack data
+            [vr, vi] = deal(xx{:});
+            n = length(idx);
+            nn = length(vr);
+            
+            %% evaluate Hessian of voltage magnitude^2 function
+            if length(lam) == n     %% upper bound or equality
+                dlam = sparse(idx, idx, 2*lam, nn, nn);
+            else                    %% doubly bounded (use lam_ub-lam_lb)
+                dlam = sparse(idx, idx, 2*(lam(n+1:2*n) - lam(1:n)), nn, nn);
+            end
+
+            %% construct Hessian
+            zz = sparse(nn, nn);
+            d2G = [dlam zz; zz dlam];
+        end
+
+        function add_opf_constraints(obj, asm, om, mpc, mpopt)
+            %% define constants
+            [PQ, PV, REF, NONE, BUS_I, BUS_TYPE, PD, QD, GS, BS, BUS_AREA, VM, ...
+                VA, BASE_KV, ZONE, VMAX, VMIN, LAM_P, LAM_Q, MU_VMAX, MU_VMIN] = idx_bus;
+
+            %% voltage angle reference constraint
+            refs = find(mpc.bus(:, BUS_TYPE) == REF);
+            varef = mpc.bus(refs, VA) * pi/180;
+            fcn_vref = @(xx)va_fcn(obj, xx, refs, varef);
+            hess_vref = @(xx, lam)va_hess(obj, xx, lam, refs);
+            om.add_nln_constraint('Vref', length(refs), 1, fcn_vref, hess_vref, {'Vr', 'Vi'});
+
+            %% fixed voltage magnitudes
+            veq = find(mpc.bus(:, VMIN) == mpc.bus(:, VMAX));
+            nveq = length(veq);
+            if nveq
+                fcn_vm2eq = @(xx)vm2_fcn(obj, xx, veq, mpc.bus(veq, VMAX).^2);
+                hess_vm2eq = @(xx, lam)vm2_hess(obj, xx, lam, veq);
+                om.add_nln_constraint('Veq', nveq, 1, fcn_vm2eq, hess_vm2eq, {'Vr', 'Vi'});
+            end
+            om.userdata.veq = veq;
+
+            %% voltage magnitude limits
+            viq = find(mpc.bus(:, VMIN) ~= mpc.bus(:, VMAX));
+            nviq = length(viq);
+            if nviq
+                fcn_vlim = @(xx)vm2_fcn(obj, xx, viq, ...
+                        {mpc.bus(viq, VMIN).^2, mpc.bus(viq, VMAX).^2} );
+                hess_vlim = @(xx, lam)vm2_hess(obj, xx, lam, viq);
+                om.add_nln_constraint({'Vmin', 'Vmax'}, [nviq;nviq], 0, fcn_vlim, hess_vlim, {'Vr', 'Vi'});
+            end
+            om.userdata.viq = viq;
         end
     end     %% methods
 end         %% classdef
