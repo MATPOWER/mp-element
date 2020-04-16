f829280269568bf2aa06fe6c44404dfc2c78422a
Ray Zimmerman <rz10@cornell.edu>
2020-04-16 17:21:31 -0600

    Implement power flow for accs_aggregate.

--- accs_aggregate.m
+++ accs_aggregate.m
@@ -21,5 +21,58 @@ classdef accs_aggregate < acc_aggregate% & accs_model
             hess_mis = @(x, lam)opf_power_balance_hess(obj, x, lam);
             om.add_nln_constraint({'Pmis', 'Qmis'}, [nn;nn], 1, fcn_mis, hess_mis);
         end
+
+
+        %%-----  PF methods  -----
+        function x = vz2pfx(obj, vr, vi, zr, zi, t)
+            pqv = [t.pq; t.pv];
+            x = [vr(pqv); vi(pqv)];
+        end
+
+        function [v_, z_] = pfx2vz(obj, x, vr, vi, zr, zi, t)
+            pqv = [t.pq; t.pv];
+            vr(pqv) = x(1:t.npv+t.npq);
+            vi(pqv) = x(t.npv+t.npq+1:end);
+            v_ = vr + 1j * vi;
+            z_ = zr + 1j * zi;
+        end
+
+        function [F, J] = power_flow_equations(obj, x, vr, vi, zr, zi, t)
+            %% index vector
+            pqv = [t.pq; t.pv];
+
+            %% update model state ([v_; z_]) from power flow state (x)
+            [v_, z_] = pfx2vz(obj, x, vr, vi, zr, zi, t);
+
+            %% incidence matrix
+            C = obj.C;
+
+            %% Jacobian
+            if nargout > 1
+                %% get port power injections with derivatives
+                [S, Svr, Svi] = obj.port_inj_power([v_; z_], 1);
+                dV2_dVr = sparse(1:t.npv, t.npq+(1:t.npv), 2*real(v_(t.pv)), t.npv, t.npv+t.npq);
+                dV2_dVi = sparse(1:t.npv, t.npq+(1:t.npv), 2*imag(v_(t.pv)), t.npv, t.npv+t.npq);
+
+                SSvr = C * Svr;
+                SSvi = C * Svi;
+                J = [   real(SSvr(pqv,  pqv)) real(SSvi(pqv,  pqv));
+                        imag(SSvr(t.pq, pqv)) imag(SSvi(t.pq, pqv));
+                        dV2_dVr dV2_dVi ];
+            %     SSzr = C * Szr;
+            %     SSzi = C * Szi;
+            %     J = [
+            %         real(SSvr(pvq,  pvq)) real(SSvi(pvq,  t.pq)) real(SSzr(pvq,  :)) real(SSzi(pvq,  :));
+            %         imag(SSvr(t.pq, pvq)) imag(SSvi(t.pq, t.pq)) imag(SSzr(t.pq, :)) imag(SSzi(t.pq, :))  ];
+            else
+                %% get port power injections (w/o derivatives)
+                S = obj.port_inj_power([v_; z_], 1);
+            end
+
+            %% nodal power balance + voltage magnitude mismatch
+            SS = C * S;
+            vmm = v_(t.pv) .* conj(v_(t.pv)) - vr(t.pv).^2 - vi(t.pv).^2;
+            F = [real(SS(pqv)); imag(SS(t.pq)); vmm];
+        end
     end     %% methods
 end         %% classdef
