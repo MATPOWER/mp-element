16638073ba77fb17a7496c97819c79ed34cf85ad
Ray Zimmerman <rz10@cornell.edu>
2020-11-25 17:23:05 -0600

    Add external/internal format conversion to mp_data_mpc2.

--- lib/mp_data_mpc2.m
+++ lib/mp_data_mpc2.m
@@ -11,6 +11,22 @@ classdef mp_data_mpc2 < mp_data
             obj.mpc = loadcase(m);
         end
 
+        function obj = ext2int(obj, mpopt)
+            if ~isfield(obj.mpc, 'order') || obj.mpc.order.state == 'e'
+                obj.mpc = ext2int(obj.mpc, mpopt);
+            else
+%                 warning('mp_data_mpc2/ext2int: data model already in internal format');
+            end
+        end
+
+        function obj = int2ext(obj, mpopt)
+            if isfield(obj.mpc, 'order') && obj.mpc.order.state == 'i'
+                obj.mpc = int2ext(obj.mpc, mpopt);
+            else
+%                 warning('mp_data_mpc2/int2ext: data model already in external format');
+            end
+        end
+
         function display(obj)
             class(obj)
             obj.mpc
