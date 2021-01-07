7e849b1cda6743a991e35da5d824e516dca45460
Ray Zimmerman <rz10@cornell.edu>
2021-01-07 16:08:02 -0600

    Merge remaining code from nlp/dcopf_solver_mpe() into opf_execute_mpe().

--- lib/opf_execute_mpe.m
+++ lib/opf_execute_mpe.m
@@ -1,6 +1,9 @@
 function [results, success, raw] = opf_execute_mpe(opf, mpopt)
 
-om = opf.mm;
+%% unpack data
+mm = opf.mm;
+results = opf.dm.mpc;
+lambda = mm.soln.lambda;
 
 %%-----  setup  -----
 %% options
@@ -9,7 +12,10 @@ alg = upper(mpopt.opf.ac.solver);
 vcart = ~dc && mpopt.opf.v_cartesian;
 
 %% get indexing
-[vv, ll, nne, nni] = om.get_idx();
+[vv, ll, nne, nni] = mm.get_idx();
+
+%% problem dimensions
+ny = mm.getN('var', 'y');   %% number of piece-wise linear costs
 
 if mpopt.verbose > 0
     v = mpver('all');
@@ -21,7 +27,16 @@ if dc
     if mpopt.verbose > 0
         fprintf(' -- DC Optimal Power Flow\n');
     end
-    [results, success, raw] = dcopf_solver_mpe(opf, mpopt);
+
+    mu = struct( ...
+      'var', struct('l', lambda.lower, 'u', lambda.upper), ...
+      'lin', struct('l', lambda.mu_l, 'u', lambda.mu_u) );
+
+    pimul = [ ...
+      mu.lin.l - mu.lin.u;
+     -ones(ny>0, 1);    %% dummy entry corresponding to linear cost row in A (in MINOS)
+      mu.var.l - mu.var.u;
+    ];
 else
     %%-----  run AC OPF solver  -----
     if mpopt.verbose > 0
@@ -54,8 +69,54 @@ else
                 error('opf_execute: MPOPT.opf.ac.solver = ''%s'' requires Artelys Knitro (see https://www.artelys.com/solvers/knitro/)', alg);
             end
     end
-    [results, success, raw] = nlpopf_solver_mpe(opf, mpopt);
+    [F_BUS, T_BUS, BR_R, BR_X, BR_B, RATE_A, RATE_B, RATE_C, ...
+        TAP, SHIFT, BR_STATUS, PF, QF, PT, QT, MU_SF, MU_ST, ...
+        ANGMIN, ANGMAX, MU_ANGMIN, MU_ANGMAX] = idx_brch;
+
+    %% problem dimensions
+    nb = size(results.bus, 1);      %% number of buses
+    nl = size(results.branch, 1);   %% number of branches
+
+    muSf = results.branch(:, MU_SF) * results.baseMVA;
+    muSt = results.branch(:, MU_ST) * results.baseMVA;
+
+    %% package up results
+    nlnN = 2*nb + 2*nl;     %% because muSf and muSt are nl x 1, not nl2 x 1
+
+    %% extract multipliers for nonlinear constraints
+    kl = find(lambda.eqnonlin(1:2*nb) < 0);
+    ku = find(lambda.eqnonlin(1:2*nb) > 0);
+    nl_mu_l = zeros(nlnN, 1);
+    nl_mu_u = [zeros(2*nb, 1); muSf; muSt];
+    nl_mu_l(kl) = -lambda.eqnonlin(kl);
+    nl_mu_u(ku) =  lambda.eqnonlin(ku);
+
+    if isfield(lambda, 'ineqnonlin')
+        lam_nli = lambda.ineqnonlin;
+    else
+        lam_nli = [];
+    end
+
+    mu = struct( ...
+      'var', struct('l', lambda.lower, 'u', lambda.upper), ...
+      'nln', struct('l', nl_mu_l, 'u', nl_mu_u), ...
+      'nle', lambda.eqnonlin, ...
+      'nli', lam_nli, ...
+      'lin', struct('l', lambda.mu_l, 'u', lambda.mu_u) );
+
+    pimul = [ ...
+      mu.nln.l - mu.nln.u;
+      mu.lin.l - mu.lin.u;
+     -ones(ny>0, 1);    %% dummy entry corresponding to linear cost row in A (in MINOS)
+      mu.var.l - mu.var.u;
+    ];
 end %% if dc
+
+[results.om, results.x, results.mu, results.f] = deal(mm, mm.soln.x, mu, mm.soln.f);
+success = opf.success;
+raw = struct('xr', mm.soln.x, 'pimul', pimul, 'info', mm.soln.eflag, 'output', mm.soln.output);
+
+
 if ~isfield(raw, 'output') || ~isfield(raw.output, 'alg') || isempty(raw.output.alg)
     raw.output.alg = alg;
 end
@@ -72,13 +133,12 @@ if success
       end
       %% compute g, dg, unless already done by post-v4.0 MINOPF or TSPOPF
       if ~isfield(raw, 'dg')
-        mpc = om.get_mpc();
-        [g, geq, dg, dgeq] = nlp_consfcn(om, results.x);
+        [g, geq, dg, dgeq] = nlp_consfcn(mm, results.x);
         raw.g = [ geq; g];
         raw.dg = [ dgeq'; dg'];   %% true Jacobian organization
       end
       %% compute df, d2f
-      [f, df, d2f] = nlp_costfcn(om, results.x);
+      [f, df, d2f] = nlp_costfcn(mm, results.x);
       raw.df = df;
       raw.d2f = d2f;
     end
@@ -100,10 +160,10 @@ else
 end
 
 %% assign values and limit shadow prices for variables
-om_var_order = om.get('var', 'order');
+om_var_order = mm.get('var', 'order');
 for k = 1:length(om_var_order)
   name = om_var_order(k).name;
-  if om.getN('var', name)
+  if mm.getN('var', name)
     idx = vv.i1.(name):vv.iN.(name);
     results.var.val.(name) = results.x(idx);
     results.var.mu.l.(name) = results.mu.var.l(idx);
@@ -112,10 +172,10 @@ for k = 1:length(om_var_order)
 end
 
 %% assign shadow prices for linear constraints
-om_lin_order = om.get('lin', 'order');
+om_lin_order = mm.get('lin', 'order');
 for k = 1:length(om_lin_order)
   name = om_lin_order(k).name;
-  if om.getN('lin', name)
+  if mm.getN('lin', name)
     idx = ll.i1.(name):ll.iN.(name);
     results.lin.mu.l.(name) = results.mu.lin.l(idx);
     results.lin.mu.u.(name) = results.mu.lin.u(idx);
@@ -124,57 +184,62 @@ end
 
 %% assign shadow prices for nonlinear constraints
 if ~dc
-  om_nle_order = om.get('nle', 'order');
+  om_nle_order = mm.get('nle', 'order');
   for k = 1:length(om_nle_order)
     name = om_nle_order(k).name;
-    if om.getN('nle', name)
+    if mm.getN('nle', name)
       results.nle.lambda.(name) = results.mu.nle(nne.i1.(name):nne.iN.(name));
     end
   end
 
-  om_nli_order = om.get('nli', 'order');
+  om_nli_order = mm.get('nli', 'order');
   for k = 1:length(om_nli_order)
     name = om_nli_order(k).name;
-    if om.getN('nli', name)
+    if mm.getN('nli', name)
       results.nli.mu.(name) = results.mu.nli(nni.i1.(name):nni.iN.(name));
     end
   end
 end
 
 %% assign values for components of quadratic cost
-om_qdc_order = om.get('qdc', 'order');
+om_qdc_order = mm.get('qdc', 'order');
 for k = 1:length(om_qdc_order)
   name = om_qdc_order(k).name;
-  if om.getN('qdc', name)
-    results.qdc.(name) = om.eval_quad_cost(results.x, name);
+  if mm.getN('qdc', name)
+    results.qdc.(name) = mm.eval_quad_cost(results.x, name);
   end
 end
 
 %% assign values for components of general nonlinear cost
-om_nlc_order = om.get('nlc', 'order');
+om_nlc_order = mm.get('nlc', 'order');
 for k = 1:length(om_nlc_order)
   name = om_nlc_order(k).name;
-  if om.getN('nlc', name)
-    results.nlc.(name) = om.eval_nln_cost(results.x, name);
+  if mm.getN('nlc', name)
+    results.nlc.(name) = mm.eval_nln_cost(results.x, name);
   end
 end
 
 %% assign values for components of legacy user cost
-om_cost_order = om.get('cost', 'order');
+om_cost_order = mm.get('cost', 'order');
 for k = 1:length(om_cost_order)
   name = om_cost_order(k).name;
-  if om.getN('cost', name)
-    results.cost.(name) = om.eval_legacy_cost(results.x, name);
+  if mm.getN('cost', name)
+    results.cost.(name) = mm.eval_legacy_cost(results.x, name);
   end
 end
 
 %% if single-block PWL costs were converted to POLY, insert dummy y into x
 %% Note: The "y" portion of x will be nonsense, but everything should at
 %%       least be in the expected locations.
-pwl1 = om.get_userdata('pwl1');
+gen_dme = opf.dm.elm_by_name('gen');
+if ~isempty(gen_dme) && ~isempty(gen_dme.pwl1)
+    pwl1 = gen_dme.pwl1;
+else
+    pwl1 = [];
+end
 if ~isempty(pwl1)
   %% get indexing
-  vv = om.get_idx();
+  vv = mm.get_idx();
   if dc
     nx = vv.iN.Pg;
   else
