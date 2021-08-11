0c2350ef2f653a9322bb64ec7199910dfaf9cfb3
Ray Zimmerman <rz10@cornell.edu>
2021-08-11 16:52:08 -0500

    Add license lines to run_* functions.

--- lib/run_mp.m
+++ lib/run_mp.m
@@ -1,5 +1,13 @@
 function task = run_mp(tag, m, mpopt, fname, solvedcase)
 
+%   MATPOWER
+%   Copyright (c) 2021, Power Systems Engineering Research Center (PSERC)
+%   by Ray Zimmerman, PSERC Cornell
+%
+%   This file is part of MATPOWER.
+%   Covered by the 3-clause BSD License (see LICENSE file for details).
+%   See https://matpower.org for more info.
+
 if nargin < 5
     solvedcase =  '';
     if nargin < 4
