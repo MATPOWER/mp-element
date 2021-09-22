4ce58564e44611654dcd533b5fd60ff3eb63456d
Ray Zimmerman <rz10@cornell.edu>
2021-09-22 14:53:01 -0500

    Simplify nme_branch_acc angle difference constraint functions
    Now based on the existing voltage constraint functions in mp_form_acc.

--- lib/nme_branch_acc.m
+++ lib/nme_branch_acc.m
@@ -12,31 +12,23 @@ classdef nme_branch_acc < nme_branch_ac & mp_form_acc
 
     methods
         function [h, dh] = ang_diff_fcn(obj, xx, Aang, lang, uang)
-            %% unpack data
-            [vr, vi] = deal(xx{:});
-
-            %% compute branch angle difference
-            va = angle(vr + 1j* vi);
+            if nargout > 1
+                [va, dva] = obj.va_fcn(xx, [], 0);
+                Adva = Aang * dva;
+                dh = [ -Adva; Adva ];
+            else
+                va = obj.va_fcn(xx, [], 0);
+            end
             Ax = Aang * va;
             h = [ lang - Ax;
                   Ax - uang ];
-
-            if nargout > 1
-                %% compute partials of branch angle difference w.r.t vr and vi
-                nn = length(vr);
-                vm2 = vr.^2 + vi.^2;
-                Aangdva_dvr = Aang * sparse(1:nn, 1:nn, -vi./vm2, nn, nn);
-                Aangdva_dvi = Aang * sparse(1:nn, 1:nn,  vr./vm2, nn, nn);
-                dh = [ -Aangdva_dvr -Aangdva_dvi;   %% h w.r.t vr, vi
-                        Aangdva_dvr  Aangdva_dvi ];
-            end
         end
 
         function d2H = ang_diff_hess(obj, xx, lambda, Aang)
             %% unpack data
             [vr, vi] = deal(xx{:});
             nn = length(vr);
-            
+
             %% evaluate Hessian of voltage magnitude^2 function
             n = length(lambda) / 2;
             if n
@@ -45,17 +37,7 @@ classdef nme_branch_acc < nme_branch_ac & mp_form_acc
                 lam = zeros(0,1);
             end
 
-            vr2 = vr.^2;
-            vi2 = vi.^2;
-
-            lam_vm4 = (Aang' * lam) ./ (vr2 + vi2).^2;
-
-            h_rr = sparse(1:nn, 1:nn, 2 * lam_vm4 .*  vr .* vi,   nn, nn);
-            h_ri = sparse(1:nn, 1:nn,     lam_vm4 .* (vi2 - vr2), nn, nn);
-
-            %% construct Hessian
-            d2H = [ h_rr  h_ri;
-                    h_ri -h_rr ];
+            d2H = obj.va_hess(xx, Aang' * lam, []);
         end
 
         function obj = opf_add_constraints(obj, mm, nm, dm, mpopt)
