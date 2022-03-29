c944802a14c9ed3191274c560a87e1b9ed34f247
Ray Zimmerman <rz10@cornell.edu>
2022-03-29 16:04:34 -0500

    Implement node_test tests via MATPOWER extension: mpx_node_test

---
+++ lib/t/mpx_node_test.m
@@ -0,0 +1,36 @@
+classdef mpx_node_test < mp_extension
+
+%   MATPOWER
+%   Copyright (c) 2022, Power Systems Engineering Research Center (PSERC)
+%   by Ray Zimmerman, PSERC Cornell
+%
+%   This file is part of MATPOWER.
+%   Covered by the 3-clause BSD License (see LICENSE file for details).
+%   See https://matpower.org for more info.
+
+%     properties
+%     end     %% properties
+
+    methods
+        function dmc_class = dm_converter_class(obj, dmc_class, fmt, mpopt)
+            dmc_class = @mp_dm_converter_mpc2_node_test;
+        end
+
+        function dm_class = data_model_class(obj, dm_class, task_tag, mpopt)
+            dm_class = @mp_data_node_test;
+        end
+
+        function nm_class = network_model_class(obj, nm_class, task_tag, mpopt)
+            nm_class = @mp_network_acp_node_test;
+        end
+
+        function mm_class = math_model_class(obj, mm_class, task_tag, mpopt)
+            switch task_tag
+                case {'PF'}
+                    mm_class = @mp_math_pf_acps_node_test;
+                case {'OPF'}
+                    mm_class = @mp_math_opf_acps_node_test;
+            end
+        end
+    end     %% methods
+end         %% classdef
