aab07c80ba2a28201a2685bbc836509282540316
Ray Zimmerman <rz10@cornell.edu>
2022-11-29 18:21:43 -0600

    Move octave5_is_method_override() function into mp_mapped_array.m.
    This is the only place it is being used (for Octave 5.x compatibility).

--- lib/mp_mapped_array.m
+++ lib/mp_mapped_array.m
@@ -298,3 +298,28 @@ classdef mp_mapped_array < handle
         end
     end     %% methods
 end         %% classdef
+
+function TorF = octave5_ismethod_override(obj, name)
+    %OCTAVE5_ISMETHOD_OVERRIDE  Replacement for built-in ISMETHOD() for Octave < 6
+    %
+    %   In GNU Octave versions before 6.0 the built-in ISMETHOD does not find
+    %   methods defined in the classdef file. This implements a replacement
+    %   that does. Typical usage:
+    %
+    %       if have_feature('octave', 'vnum') < 6
+    %           my_ismethod = @(o, n)octave5_ismethod_override(o, n);
+    %       else
+    %           my_ismethod = @ismethod;
+    %       end
+    %
+    %   See also ISMETHOD.
+
+    meths = meta.class.fromName(class(obj)).MethodList;
+    for k = 1:length(meths)
+        if strcmp(meths{k}.Name, name)
+            TorF = true;
+            return;
+        end
+    end
+    outTorF = false;
+end
